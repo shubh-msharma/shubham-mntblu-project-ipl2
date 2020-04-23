b88eaf2a988051ea19a6cb5e96ce2d29ebf24269
shubh-msharma <shubhamsharma4539@gmail.com>
2020-04-23 14:51:27 +0530

    sfsdfsdfds

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const route = require('./routes/routes');
@@ -7,6 +8,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3004 ;
 
+app.use(cors())
 app.use(route);
 
 app.use(express.static(path.join(__dirname,"./public")));
